f991f3f286b6c4baec0313b6de342c9376316995
optyj59 <optyj59@naver.com>
2023-03-30 20:33:24 +0900

    first

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from typing import List
 # Skeleton code for even_list
 def even_list(int_list: List[int]) -> List[int]:
     """   
-    Determines if a number is even and return an even list.
+ Determines if a number is even and return an even list.
     Args:        int_list: A list of integer.
     Returns:        A list of even integers.
     """
